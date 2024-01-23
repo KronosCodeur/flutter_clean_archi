68b2172cc918d708c281d52ea004afd1819422c0
KronosCodeur <piraliarris@gmail.com>
2024-01-23 01:09:02 +0000

    posts list page

---
+++ lib/core/features/posts/ui/pages/post_list_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_clean_archi/core/error/failure.dart';
+import 'package:flutter_clean_archi/core/features/posts/data/models/post_model.dart';
+import 'package:flutter_clean_archi/core/features/posts/ui/manager/article_manager.dart';
+import 'package:flutter_clean_archi/core/features/posts/ui/widgets/post_list_item.dart';
+import 'package:provider/provider.dart';
+
+class PostListPage extends StatelessWidget {
+  const PostListPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<PostModel>? posts = Provider.of<PostManager>(context).posts;
+    Failure? failure = Provider.of<PostManager>(context).failure;
+    late Widget widget;
+    if (posts != null) {
+      widget = ListView.builder(
+          itemCount: posts.length,
+          itemBuilder: (context, index) {
+            return PostListItem(post: posts[index]);
+          });
+    } else if (failure != null) {
+      widget = Center(
+        child: Text(failure.errorMessage),
+      );
+    } else {
+      widget = const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Lists of posts'),
+      ),
+      body: widget,
+    );
+  }
+}
